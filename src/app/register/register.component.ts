c04a6eda23fbf157d18372eae1e15c1bf3ea2845
lukakuncak <lukakuncak1@gmail.com>
2024-07-13 17:47:04 +0200

    add axios and repolish register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -9,33 +9,36 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   registrationForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
     this.registrationForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       username: ['', Validators.required],
-      password: ['', Validators.required],
-      repeatPassword: ['', Validators.required],
-      isStudent: [false],
-      grade: [{ value: '', disabled: true }, [Validators.required, Validators.min(1), Validators.max(8)]]
-    });
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      repeatPassword: ['', Validators.required]
+    }, { validator: this.passwordMatchValidator });
+  }
 
-    this.registrationForm.get('isStudent')?.valueChanges.subscribe(isStudent => {
-      const gradeControl = this.registrationForm.get('grade');
-      if (isStudent) {
-        gradeControl?.enable();
-      } else {
-        gradeControl?.disable();
-      }
-    });
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password');
+    const repeatPassword = form.get('repeatPassword');
+    if (password && repeatPassword && password.value !== repeatPassword.value) {
+      repeatPassword.setErrors({ passwordMismatch: true });
+    } else {
+      repeatPassword.setErrors(null);
+    }
+    return null;
   }
 
   onSubmit() {
     if (this.registrationForm.valid) {
-      console.log(this.registrationForm.value);
+      // Handle form submission
+      console.log('Form Submitted', this.registrationForm.value);
     }
   }
 }
